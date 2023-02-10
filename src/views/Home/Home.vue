4326a27ae140cdd5e9e576aef5c11f54907a21cf
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-02-10 12:07:58 -0300

    style(components): added styles to the layout and search form
    added new form for search names and his respective style

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>Home</h1>
+  <section>
+    <form @submit.prevent="onSubmit">
+      <div class="search-container">
+        <input
+          id="search-input"
+          v-model="query"
+          type="text"
+          placeholder="Pikachu..."
+        >
+        <button
+          id="submit-button"
+          :disabled="query === ''"
+          type="submit"
+        >
+          <img
+            id="search-icon"
+            src="@/assets/svg/search-white.svg"
+            alt="search"
+          >
+        </button>
+      </div>
+    </form>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const query = ref('');
+
+const onSubmit = (): void => {
+  console.log(query.value);
+}
+</script>
+
+<style src="./_Home.scss" lang="scss" scoped></style>
