e5f6d66bce8d91488d0ff7a2910489e4ca6e31dd
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-02-11 12:25:26 -0300

    feat(components): added paginantion
    added pagination component with 3 items range

--- src/views/ListAll/ListAll.vue
+++ src/views/ListAll/ListAll.vue
@@ -9,31 +9,59 @@
       <span>Sorry! We are having some trouble</span>
     </p>
 
-    <Render
-      v-else-if="items"
-      :items="items"
-    />
+    <div v-else-if="items && items.length">
+      <Pagination
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @update-current-page="updatePage"
+      />
+
+      <Render
+        :items="items"
+      />
+
+      <Pagination
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @update-current-page="updatePage"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { fetch } from '../../service/api';
 import { Pokemon } from '../../types/Pokemon';
 
 import Render from '../../components/Render/Render.vue';
 import Loading from '../../components/Loading/Loading.vue';
+import Pagination from '../../components/Pagination/Pagination.vue';
+
+const totalPages = computed(() => Math.ceil(count.value / 12));
 
 const error = ref<boolean>(false);
 const items = ref<Pokemon[]>();
 const loading = ref<boolean>(false);
 const disabled = ref<boolean>(false);
+const currentPage = ref<number>(1);
+const count = ref<number>(1);
+const offset = ref<number>(0);
+
+const limit = 12;
+
+const updatePage = (page: number): void => {
+  currentPage.value = page;
+  offset.value = (currentPage.value - 1) * 12;
+  getAll();
+};
 
 const getAll = async () => {
+  items.value = [];
   loading.value = true;
   disabled.value = true;
 
-  await fetch.getAllPokemon()
+  await fetch.getAllPokemon(offset.value, limit)
   .then(async (response) => {
     if (!response.error) {
       const pokemonList = await Promise.all(
@@ -44,6 +72,7 @@ const getAll = async () => {
         )
       );
 
+      count.value = response.data.count;
       error.value = false;
       items.value = pokemonList;
       loading.value = false;
